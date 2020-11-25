af44cfd053cb8b741a4b897cc45322064f0d3928
sem-aa <66129330+sem-aa@users.noreply.github.com>
2020-11-26 00:29:17 +0200

    Делает таймер

---
+++ main.js
@@ -0,0 +1,44 @@
+const refs = {
+  days: document.querySelector('[data-value="days"]'),
+  hours: document.querySelector('[data-value="hours"]'),
+  mins: document.querySelector('[data-value="mins"]'),
+  secs: document.querySelector('[data-value="secs"]'),
+};
+
+class CountdownTimer {
+  constructor(date) {
+    this.selector = date.selector;
+    this.targetDate = date.targetDate;
+    this.deltaTime = 0;
+  }
+
+  pad(value) {
+    return String(value).padStart(2, "0");
+  }
+
+  getTimeComponents(time) {
+    const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = this.pad(
+      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    );
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+    refs.days.textContent = days;
+    refs.hours.textContent = hours;
+    refs.mins.textContent = mins;
+    refs.secs.textContent = secs;
+  }
+
+  start() {
+    setInterval(() => {
+      const currentTime = Date.now();
+      this.deltaTime = this.targetDate - currentTime;
+      this.getTimeComponents(this.deltaTime);
+    }, 1000);
+  }
+}
+
+new CountdownTimer({
+  selector: "#timer-1",
+  targetDate: new Date("Mar 12, 2021"),
+}).start();
